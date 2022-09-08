4900109748937796dabe44f56358ef86b216de73
EmmanuelPriest <emmanuelchukwujekwu87@gmail.com>
2022-09-08 11:14:31 -0700

    monty simple operators and main

--- monty.h
+++ monty.h
@@ -42,13 +42,14 @@ typedef struct instruction_s
 
 
 /**
- * struct bus_s - variables -args, file, line content
- * @arg: value
- * @file: pointer to monty file
- * @content: line content
- * @lifi: flag change stack <-> queue
- * Description: carries values through the program
- */
+* struct bus_s - variables -args, file, line content
+* @arg: value
+* @file: pointer to monty file
+* @content: line content
+* @lifi: flag change stack <-> queue
+*
+* Description: carries values through the program
+*/
 typedef struct bus_s
 {
 	char *arg;
